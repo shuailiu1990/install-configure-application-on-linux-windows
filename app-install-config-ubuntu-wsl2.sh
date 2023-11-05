811df85280b6a3294750a17b2ea0d7837b28da9d
shuailiu1990 <yuyefeng28@163.com>
2023-11-05 22:50:59 +0800

    add the command to intall zsh and oh my zsh

--- app-install-config-ubuntu-wsl2.sh
+++ app-install-config-ubuntu-wsl2.sh
@@ -1,3 +1,17 @@
 #/bin/bash
 
-# install zsh and oh-my-zsh, and configure them 
+#--------    zsh    --------   
+# Install zsh 
+sudo apt update
+sudo apt install zsh-y
+zsh --version
+# Set zsh as the default shell
+chsh -s $(which zsh)
+# Install oh my zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# Install some plugins of oh my zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+# The configrations on zsh are in the file .zshrc
