f472e48a81815ec0e98b0fd68f8a6d22aab802fe
shuailiu1990 <yuyefeng28@163.com>
2023-11-05 22:57:20 +0800

    add the links to intall zsh and oh my zsh

--- app-install-config-ubuntu-wsl2.sh
+++ app-install-config-ubuntu-wsl2.sh
@@ -15,4 +15,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
-# The configrations on zsh are in the file .zshrc
+# The configrations on zsh are in the file ~/.zshrc
