a5498a12a175d0e0fdd6b44d918013bd150c34b6
shuailiu1990 <yuyefeng28@163.com>
2023-12-19 21:01:44 +0800

    add commands to install compelling environment and improve other install commands

--- app-install-ubuntu.sh
+++ app-install-ubuntu.sh
@@ -9,60 +9,188 @@ if [ ! -d ~/opt ]; then
 fi
 
 #--------    zsh    --------
-# The reference links: https://phoenixnap.com/kb/install-zsh-ubuntu, https://gist.github.com/n1snt/454b879b8f0b7995740ae04c5fb5b7df
+# The reference link: https://phoenixnap.com/kb/install-zsh-ubuntu, https://gist.github.com/n1snt/454b879b8f0b7995740ae04c5fb5b7df
 
-# Enter the decision to determine whether to install zsh in command line 
-read -p "Install zsh? (y/n):" flag_install_zsh 
+if [ ! which zsh 1>/dev/null 2>&1 ]; then
+    echo zsh is not installed!
 
-if [ $flag_install_zsh = "y" ]; then
-    # Enter the decision to determine whether to install zsh from sourcecode in command line 
-    read -p "Install zsh from sourcecode without sudo permission (y/n):" flag_way_install_zsh 
-    if [ $flag_way_install_zsh = "y" ]; then 
-        # Install zsh from sourcecode. If the source code cannt be downloaded, we can copy them from a computer to the target one 
-        if [ ! -d ~/download/zsh*.tar.gz ]; then
-            wget https://sourceforge.net/projects/zsh/files/latest/download -O ~/download/zsh.tar.gz
-        fi &&
-        
-        cd ~/download &&
-        
-        if [ ! -d ./zsh ]; then
-            mkdir zsh
-        fi &&
-        
-        tar -xf zsh.tar.gz -C ./zsh --strip-components 1 &&
-        cd ./zsh && 
-        
-        ./configure --prefix=$HOME/opt &&
-        make && 
-        make install &&
-        
-        # Install oh my zsh
-        rm -rf ~/.oh-my-zsh &&
-        git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh 
-        # Some configrations need to be written in the file ~/.bash_profile and ~/.bashrc
-    else
-        # Install zsh by sudo apt 
-        sudo apt update && 
-        sudo apt install zsh-y && 
-        
-        # Set zsh as the default shell
-        chsh -s $(which zsh) &&
-
-        # Download and install oh my zsh. If it cannot be downloaded, we can copy it from a computer to the target one.
-        rm -rf ~/.oh-my-zsh &&
-        sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" 
-    fi
+    # Enter the decision to determine whether to install zsh in command line 
+    read -p "Install zsh? (y/n):" flag_install_zsh 
     
-    # Install some plugins of oh my zsh. If the plugins cannot be downloaded, we can copy them from a computer to the target one.
+    if [ $flag_install_zsh = "y" ]; then
+        # Enter the decision to determine whether to install zsh from sourcecode in command line 
+        read -p "Install zsh from sourcecode without sudo permission (y/n):" flag_way_install_zsh_without_sudo 
+        
+        if [ $flag_way_install_zsh_without_sudo = "y" ]; then 
+            # Install zsh from sourcecode without sudo permission. If the source code cannt be downloaded, we can copy them from a computer to the target one. 
+            if [ ! -e ~/download/zsh*.tar.gz ]; then
+                wget https://sourceforge.net/projects/zsh/files/latest/download -O ~/download/zsh.tar.gz
+            fi &&
+            
+            cd ~/download &&
+            
+            if [ ! -d ./zsh ]; then
+                mkdir zsh
+            fi &&
+            
+            tar -xf zsh.tar.gz -C ./zsh --strip-components 1 &&
+            cd ./zsh && 
+            ./configure --prefix=$HOME/opt/zsh &&
+            make && 
+            make install 
+            # Some configrations need to be written in the file ~/.bash_profile and ~/.bashrc
+        else
+            # Install zsh by sudo permission 
+            sudo apt update && 
+            sudo apt install zsh-y && 
+            
+            # Set zsh as the default shell
+            chsh -s $(which zsh) 
+        fi
+        echo zsh is installed successuflly now!
+    fi
+else
+    echo zsh have been installed before!
+fi
+# Note that the configrations on zsh are in the file ~/.zshrc
+
+
+#--------    oh my zsh     --------
+
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo oh my zsh is not installed!
+
+    # Enter the decision to determine whether to install oh my zsh in command line 
+    read -p "Install oh my zsh? (y/n):" flag_install_oh_my_zsh 
+
+    if [ $flag_install_oh_my_zsh = "y" ]; then
+        read -p "Install on my zsh from sourcecode without sudo permission (y/n):" flag_way_install_on_my_zsh_without_sudo
+        
+        if [ $flag_way_install_on_my_zsh_without_sudo = "y" ]; then
+            # Install oh my zsh without sudo permission
+            git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+        else
+            # Install oh my zsh with sudo permission. If it cannot be downloaded, we can copy ~/.oh-my-zsh in a computer to ~/ in the target computer.
+            sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" 
+        fi
+    fi
+    echo oh my zsh is installed successuflly now!  
+else
+    echo oh my zsh has been installed before!
+fi
+
+
+#--------    Plugin of oh my zsh     --------
+
+# Enter the decision to determine whether to install plugins of oh my zsh in command line 
+read -p "Install plugins of oh my zsh? (y/n):" flag_install_plugins_oh_my_zsh 
+
+if [ $flag_install_plugins_oh_my_zsh = "y" ]; then
+    # Install some plugins of oh my zsh. If the plugins cannot be downloaded, we can copy them from ~/.oh-my-zsh/custom/plugins in a computer to ~/.oh-my-zsh/custom/plugins in the target computer.
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
     git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting &&
     git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete 
 fi
-# Note: the configrations on zsh are in the file ~/.zshrc
+echo Plugisns of oh my zsh are installed successuflly!
 
 
-#--------    vim    --------
+#--------    Compelling environment  --------
+
+# Install autoconf
+if [ ! which autoconf 1>/dev/null 2>&1 ]; then
+    echo autoconf is not installed
+    
+    read -p "Install autoconf (y/n):" flag_install_autoconf
+    if [ $flag_install_autoconf = "y" ]; then
+        read -p "Install autoconf from sourcecode without sudo permission (y/n):" flag_way_install_autoconf_without_sudo
+        
+        if [ $flag_way_install_autoconf_without_sudo = "y"]; then
+            echo dd
+        else 
+            sudo apt install autoconf
+        fi
+        echo autoconf is installed now!
+    fi
+else
+    echo autoconf has been installed before!
+fi
+
+# Install automake
+if [ ! which automake 1>/dev/null 2>&1 ]; then
+    echo automake is not installed
+    
+    read -p "Install automake (y/n):" flag_install_automake
+    if [ $flag_install_automake = "y" ]; then
+        read -p "Install automake from sourcecode without sudo permission (y/n):" flag_way_install_automake_without_sudo
+        
+        if [ $flag_way_install_automake_without_sudo = "y"]; then
+            echo dd
+        else 
+            sudo apt install automake
+        fi
+        echo automake is installed now!
+    fi
+else
+    echo automake has been installed before!
+fi
+
+# Install pkg-config
+if [ ! which pkg-config 1>/dev/null 2>&1 ]; then
+    echo pkg-config is not installed
+    
+    read -p "Install pkg-config (y/n):" flag_install_pkg_config
+    if [ $flag_install_pkg_config = "y" ]; then
+        read -p "Install pkg_config from sourcecode without sudo permission (y/n):" flag_way_install_pkg_config_without_sudo
+        
+        if [ $flag_way_install_pkg_config_without_sudo = "y"]; then
+            echo dd
+        else 
+            sudo apt install pkg-config 
+        fi
+        echo pkg-config is installed now!
+    fi
+else
+    echo pkg-config has been installed before!
+fi
+
+
+#--------    Environment or software that some plugins of vim require   --------
+# The link on installing universal ctags from sourcecode is https://github.com/universal-ctags/ctags
+
+if [ ! which ctags 1>/dev/null 2>&1 ]; then
+    echo universal ctags is not installed!
+    
+    # Install universal ctags which tagbar requires 
+    read -p "Install universal ctags? (y/n):" flag_install_universal_ctags
+    
+    if [ $flag_install_universal_ctags = "y" ]; then
+        # Enter the decision to determine whether to install universal ctags from sourcecode in command line 
+        read -p "Install ctags from sourcecode without sudo permission (y/n):" flag_way_install_universal_ctags_without_sudo 
+        
+        if [ $flag_way_install_universal_ctags_without_sudo = "y" ]; then
+            cd ~/download &&
+           
+            if [ ! -e ./ctags ]; then
+                git clone https://github.com/universal-ctags/ctags.git
+            fi &&
+    
+            cd ctags &&
+            ./autogen.sh &&
+            ./configure --prefix=$HOME/opt/ctags && # set the installation directory, defaults to /usr/local
+            make &&
+            make install 
+        else
+            sudo apt install universal-ctags
+        fi
+    fi
+    echo universal ctags is installed successuflly now!
+else
+    echo universal ctags has been installed before!
+fi
+
+
+#--------    Plugin of vim    --------
 
 # Install the vim plugins manager called vim-plug
 read -p "Install vim-plug? (y/n):" flag_install_vim_plug
@@ -78,29 +206,6 @@ if [ $flag_install_vim_plug = "y" ]; then
         # Check the state of plugins: enter :PlugStatus in vim
 fi
 
-# Install the environment or software that some vim plugins require
 
-# Install ctags which tagbar requires 
-read -p "Install universal ctags? (y/n):" flag_install_universal_ctags
-
-if [ $flag_install_universal_ctags = "y" ]; then
-    # Enter the decision to determine whether to install universal ctags from sourcecode in command line 
-    read -p "Install ctags from sourcecode without sudo permission (y/n):" flag_way_install_universal_ctags 
-    
-    if [ $flag_way_install_universal_ctags = "y" ]; then
-        if [ ! -d ~/download/zsh*.tar.gz ]; then
-        
-        fi
-        # https://github.com/universal-ctags/ctags
-        git clone https://github.com/universal-ctags/ctags.git &&
-        cd ctags &&
-        ./autogen.sh &&
-        ./configure &&  # use --prefix=/where/you/want to override installation directory, defaults to /usr/local
-         make &&
-         make install # may require extra privileges depending on where to install
-    else:
-        sudo apt install universal-ctags
-    fi
-fi
 
 
