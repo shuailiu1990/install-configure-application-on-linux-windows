fd66a248d020dbf83655499b2c70fcb56c5007aa
shuailiu1990 <yuyefeng28@163.com>
2024-01-23 22:57:58 +0800

    update

--- linux/.config/nvim/lua/config/options.lua
+++ linux/.config/nvim/lua/config/options.lua
@@ -5,3 +5,4 @@ vim.opt.tabstop = 4 -- number of spaces that a <Tab> in the file counts for
 vim.opt.shiftwidth = 4 -- number of spaces to use for each step of (auto)indent
 vim.opt.softtabstop = 4 -- number of spaces that a <Tab> counts for while performing editing operations
 vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.relativenumber = false -- disable relativenumber
