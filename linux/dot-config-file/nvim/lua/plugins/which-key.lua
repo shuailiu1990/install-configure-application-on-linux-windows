3028d87d5e9ddc1c48be1f812f39f564b1c44b75
shuailiu1990 <yuyefeng28@163.com>
2024-03-12 21:59:14 +0800

    rm lazyvim.json and update which-key.lua

--- linux/dot-config-file/nvim/lua/plugins/which-key.lua
+++ linux/dot-config-file/nvim/lua/plugins/which-key.lua
@@ -1,28 +1,30 @@
---return {
---  {
---    "folke/which-key.nvim",
---    opts = {
---      plugins = { spelling = false },
---      defaults = {
---        ["<leader>t"] = { name = "+terminal" },
---      },
---    },
---  },
---}
-
 return {
-  -- Modify which-key keys
   {
     "folke/which-key.nvim",
-    opts = function()
-      require("which-key").register({
-        ["<leader>t"] = {
-          name = "+terminal",
-        },
-        ["<leader>r"] = {
-          name = "+wrap mode",
-        },
-      })
-    end,
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>t"] = { name = "+terminal" },
+        ["<leader>r"] = { name = "+wrap mode" },
+        --["<leader>m"] = { name = "+markdown" },
+        ["<leader>d"] = { name = "+snip run operator" },
+        ["<localleader>l"] = { name = "+vimtex" },
+      },
+    },
   },
 }
+
+--return {
+--  -- Modify which-key keys
+--  {
+--    "folke/which-key.nvim",
+--    opts = function()
+--      require("which-key").register({
+--        ["<leader>t"] = { name = "+terminal" },
+--        ["<leader>r"] = { name = "+wrap mode" },
+--        ["<leader>m"] = { name = "+snip run" },
+--        ["<localLeader>m"] = { name = "+vimtex" },
+--      })
+--    end,
+--  },
+--}
