7e51b674721f4e8b513237f9d805958ba5b1ab8c
shuailiu1990 <yuyefeng28@163.com>
2024-05-11 21:52:13 +0800

    update

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,10 @@
 Invoke-Expression (&starship init powershell)
+
+Import-Module PSReadLine
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+#$ENV:STARSHIP_CONFIG = "C:\Users\刘帅\.config\starship.toml"
+
+
--- windows/install-configure-applicaiton-on-windows.ps1
+++ windows/install-configure-applicaiton-on-windows.ps1
@@ -1,9 +1,15 @@
-# Install Starship 
-#winget install --id Starship.Starship
+#Install Scoop
+#Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+#iwr -useb get.scoop.sh | iex
+#
+## Install sudo with Scoop
+#Scoop install sudo
+#
+## Install Starship with Scoop
+#Scoop install starship
+#
+## Install Neovim with Scoop
+#Scoop install neovim 
 
-# Install Scoop
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-iwr -useb get.scoop.sh | iex
-
-# Install sudo with Scoop
-Scoop install sudo
+Install-Module PowerShellGet -scope CurrentUser -Force -AllowClobber
+Install-Module PSReadLine -scope CurrentUser
